0016638d16dcfdb7f14e3bca40366b4cfac693d4
Emil A. Cherrington <emil.cherrington@gmail.com>
2025-09-18 15:56:01 -0500

    Create netcdf_to_geotif__pace_chlor_a__2025-09-18.R

---
+++ _scripts_R/netcdf_to_geotif__pace_chlor_a__2025-09-18.R
@@ -0,0 +1,18 @@
+# Batch convert 8-day PACI OCI Land VI data from NetCDF to GeoTIF
+# Data can be grabbed from the NASA OB.DAAC: https://oceandata.sci.gsfc.nasa.gov/l3/
+# or from NASA EarthData: https://search.earthdata.nasa.gov/search
+# Last updated: 18.09.2025
+
+# Load libraries
+require(ncdf4)
+require(raster)
+
+## Input directory for all PACE OCI Land VI data
+dir.nc <- 'D:/gis/temp_2025/nasa_pace_oci_chl/netcdf/'
+files.nc <- list.files(dir.nc, full.names = T, recursive = T)
+
+# Var 1: Chlorophyll a (chlor_a)
+dir.output <- 'D:/gis/temp_2025/nasa_pace_oci_chl/tif/' # 1
+for (i in 1:length(files.nc)) {
+  r.nc <- raster(files.nc[i], varname="chlor_a")
+  writeRaster(r.nc, paste(dir.output, i, '.tiff', sep = ''), format = 'GTiff', overwrite = T)}
